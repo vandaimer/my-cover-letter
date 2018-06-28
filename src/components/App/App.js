9ea32876f985b080b922a341255515d657c72656
Luiz Filipe <moresco.luiz@gmail.com>
2018-06-27 22:36:26 -0300

    Use withStyles from material-ui instead of css file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import { storage } from '../../config';
 import Skill from '../Skill';
-import './App.css';
 
 class App extends Component {
   state = {
@@ -20,14 +20,15 @@ class App extends Component {
 
   render() {
     const { projects } = this.state;
+    const { classes } = this.props;
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Luiz Filipe's Portifolio</h1>
-          <h1 className="App-title">Full-Stack Software Developer</h1>
+      <div className={classes.app}>
+        <header className={classes.header}>
+          <h1 className={classes.title}>Luiz Filipe's Portifolio</h1>
+          <h1 className={classes.title}>Full-Stack Software Developer</h1>
         </header>
-        <div className="App-intro">
+        <div className={classes.intro}>
           {projects && projects.map((project, index) => (
             <Skill
               key={index}
@@ -42,4 +43,27 @@ class App extends Component {
   }
 }
 
-export default App;
+const styles = {
+  app: {
+    textAlign: 'center',
+    backgroundColor: '#185f9b',
+  },
+  header: {
+    height: 300,
+    padding: 20,
+    color: 'white',
+  },
+  title: {
+    fontSize: '1.5em',
+  },
+  intro: {
+    fontSize: 'large',
+    background: 'linear-gradient(#185f9b, #91c5f2)',
+    padding: 16,
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    flexWrap: 'wrap',
+  }
+};
+
+export default withStyles(styles)(App);
